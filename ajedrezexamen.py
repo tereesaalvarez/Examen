3abd77a1f67054aae27f1bbd6eb71a07ac636aac
tereesaalvarez <alvdeporteresa@gmail.com>
2021-12-14 10:50:10 +0100

    def tablero

--- ajedrezexamen.py
+++ ajedrezexamen.py
@@ -4,4 +4,19 @@ import random
 import re
 import sys
 
+def tablerocerrado (fila, columna):
+    if fila ==0 and tablero [fila+1][columna] != '':
+        fallo= True
+
+    elif fila == 1:
+        if tablero [fila+1][columna] != '' and tablero[fila-1][columna] != '':
+            fallo = True
+
+        else:
+            fallo= False
+    elif fila == 2 and tablero [fila-1][columna] != '':
+        fallo = True
+    else:
+        fallo = True
+    return fallo
 
