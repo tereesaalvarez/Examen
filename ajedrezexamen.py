cbf2c2339bd56f7573c48dd749e6eb987f0189bf
tereesaalvarez <alvdeporteresa@gmail.com>
2021-12-14 11:12:46 +0100

    def cambio

--- ajedrezexamen.py
+++ ajedrezexamen.py
@@ -4,7 +4,7 @@ import random
 import re
 import sys
 
-def tablerocerrado (fila, columna, tablero):
+def tablerocerrado (fila, columna):
     if fila ==0 and tablero [fila+1][columna] != '':
         fallo= True
 
@@ -25,13 +25,13 @@ def enseñartablero (tablero):
     for tablero[contador] in tablero:
         print(tablero[contador])
 
-def movimiento(fila, columna, tablero):
+def movimiento(fila, columna):
     while fila == 1:
         if tablero[fila+1][columna] != '':
             tablero[fila-1][columna] == tablero[fila][columna]
             tablero[fila][columna]= ''
         else:
-            tablero[fila+1][columna] == tablero[fila][columna]:
+            tablero[fila+1][columna] == tablero[fila][columna]
             tablero[fila][columna]= ''
     if fila == 0:
         tablero[fila+1][columna] =  tablero[fila][columna]
@@ -39,4 +39,16 @@ def movimiento(fila, columna, tablero):
     elif fila == 2:
         tablero[fila-1][columna] = tablero[fila][columna]
         tablero [fila][columna] = ''
+
+def cambio(fila, columna):
+    if fila == 0:
+        fila += 1
+    elif fila == 1:
+        if tablero[fila+1][columna] != '':
+            fila -= 1
+        else:
+            fila+= 1
+    elif fila == 2:
+        fila -= 1
+    return fila
 
