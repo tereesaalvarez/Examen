5a16ebee9a92d349e558aa9bb0cb463ef6a77759
tereesaalvarez <alvdeporteresa@gmail.com>
2021-12-14 11:06:21 +0100

    def movimiento

--- ajedrezexamen.py
+++ ajedrezexamen.py
@@ -24,4 +24,19 @@ def enseñartablero (tablero):
     contador = 0
     for tablero[contador] in tablero:
         print(tablero[contador])
-    
+
+def movimiento(fila, columna, tablero):
+    while fila == 1:
+        if tablero[fila+1][columna] != '':
+            tablero[fila-1][columna] == tablero[fila][columna]
+            tablero[fila][columna]= ''
+        else:
+            tablero[fila+1][columna] == tablero[fila][columna]:
+            tablero[fila][columna]= ''
+    if fila == 0:
+        tablero[fila+1][columna] =  tablero[fila][columna]
+        tablero[fila][columna]= ''
+    elif fila == 2:
+        tablero[fila-1][columna] = tablero[fila][columna]
+        tablero [fila][columna] = ''
+
